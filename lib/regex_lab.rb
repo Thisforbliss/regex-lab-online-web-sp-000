caf2f9c83180ae2509b8b9e4d74ca2c7bf1e35bf
Malcolm Leake <echelon1089@gmail.com>
2020-08-11 03:21:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+  word.match(word)? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+text.scan(text)
 end
 
 def words_five_letters_long(text)
-
+text.scan
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+text.match(text) ? true : false
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(phone) ? true : false
 end
